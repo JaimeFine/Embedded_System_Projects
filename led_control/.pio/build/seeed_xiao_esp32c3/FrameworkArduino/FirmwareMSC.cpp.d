eebaf4948089915cbe2980402355af7734098fb1
Jaime Fan <jaimefine6@gmail.com>
2025-09-20 22:30:33 +0800

    Delete led_control/.pio directory

--- led_control/.pio/build/seeed_xiao_esp32c3/FrameworkArduino/FirmwareMSC.cpp.d
+++
@@ -1,5 +0,0 @@
-.pio/build/seeed_xiao_esp32c3/FrameworkArduino/FirmwareMSC.cpp.o: \
- C:/Users/13647/.platformio/packages/framework-arduinoespressif32/cores/esp32/FirmwareMSC.cpp \
- C:/Users/13647/.platformio/packages/framework-arduinoespressif32/cores/esp32/FirmwareMSC.h \
- C:/Users/13647/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.h \
- C:/Users/13647/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32c3/qio_qspi/include/sdkconfig.h
--- led_control/.pio/build/seeed_xiao_esp32c3/lib103/Adafruit NeoPixel/Adafruit_Neopixel_RP2.cpp.d
+++
@@ -1,2 +0,0 @@
-.pio/build/seeed_xiao_esp32c3/lib103/Adafruit\ NeoPixel/Adafruit_Neopixel_RP2.cpp.o: \
- .pio/libdeps/seeed_xiao_esp32c3/Adafruit\ NeoPixel/Adafruit_Neopixel_RP2.cpp
